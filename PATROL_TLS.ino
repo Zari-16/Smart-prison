5894d8da124cbfc00b21bbd472fffee5cdbacdae
Zari-16 <bm950@live.mdx.ac.uk>
2025-12-15 17:24:50 +0400

    Update PATROL_TLS.ino

--- PATROL_TLS.ino
+++ PATROL_TLS.ino
@@ -19,38 +19,22 @@ const char EDGE_API_KEY[] = "REPLACE_WITH_EDGE_API_KEY";
 DHT dht(DHTPIN, DHTTYPE);
 WiFiUDP ntpUDP; NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-void sendTelemetryTLS(String json) {
+bool sendToEdgeTLS(String jsonPayload) {
+  if (WiFi.status() != WL_CONNECTED) { WiFi.begin(ssid, pass); delay(2000); }
   WiFiSSLClient client;
-  if (!client.connect(EDGE_HOST, EDGE_PORT)) { Serial.println("TLS connect failed"); return; }
-  String req = "POST /ingest HTTP/1.1\r\nHost: " + String(EDGE_HOST) + "\r\n";
+  if (!client.connect(EDGE_HOST, EDGE_PORT)) return false;
+  String req = "POST /ingest HTTP/1.1\r\n";
+  req += "Host: " + String(EDGE_HOST) + "\r\n";
   req += "Authorization: Bearer " + String(EDGE_API_KEY) + "\r\n";
   req += "Content-Type: application/json\r\n";
-  req += "Content-Length: " + String(json.length()) + "\r\n";
+  req += "Content-Length: " + String(jsonPayload.length()) + "\r\n";
   req += "Connection: close\r\n\r\n";
-  req += json;
+  req += jsonPayload;
   client.print(req);
   unsigned long start = millis();
   while (!client.available() && millis() - start < 2000) delay(10);
-  while (client.available()) Serial.write(client.read());
+  String resp="";
+  while (client.available()) resp += (char)client.read();
   client.stop();
-}
-
-void setup(){
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
-  timeClient.begin();
-  dht.begin();
-}
-
-void loop() {
-  timeClient.update();
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
-  int gas = analogRead(MQ135_PIN);
-  int water = analogRead(WATER_PIN);
-  int pir = digitalRead(PIR_PIN);
-  String j = "{\"device\":\"patrol_unit\",\"ts\":" + String(timeClient.getEpochTime()) + ",\"temp\":" + String(temp,2) + ",\"humidity\":" + String(hum,2) + ",\"gas\":" + String(gas) + ",\"water\":" + String(water) + ",\"pir\":" + String(pir) + "}";
-  sendTelemetryTLS(j);
-  delay(5000);
+  return true;
 }
